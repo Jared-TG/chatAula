28dfbeb7501ea011ae4c4bd65378af96ddc3057e
miniCoded <agbarron20@gmail.com>
2026-08-05 08:52:19 -0700

    feat: users dtos

--- src/app/models/users/users.model.ts
+++ src/app/models/users/users.model.ts
@@ -1,10 +1,21 @@
 import * as z from 'zod';
 
-export const Users = z.object({
-	_id: z.string().trim().nonempty().nonoptional(),
-	name: z.string().trim().nonempty().nonoptional(),
+export const CreateUserEmail_PasswordRequestDTO = z.object({
 	email: z.email().nonempty().nonoptional(),
-	password: z.string().trim().nonempty().nonoptional()
+	password: z.string().trim().nonempty().nonoptional(),
 });
 
-export type Users = z.infer<typeof Users>
+export const InsertUserRequestDTO = z.object({
+	email: z.email().nonempty().nonoptional(),
+	username: z.string().nonempty().nonoptional()
+});
+
+export const UserDTO = z.object({
+	_id: z.string().nonempty().nonoptional(),
+	email: z.email().nonempty().nonoptional(),
+	username: z.string().nonempty().nonoptional()
+});
+
+export type CreateUserEmail_PasswordRequestDTO = z.infer<typeof CreateUserEmail_PasswordRequestDTO>;
+export type InsertUserRequestDTO = z.infer<typeof InsertUserRequestDTO>;
+export type UserDTO = z.infer<typeof UserDTO>;
